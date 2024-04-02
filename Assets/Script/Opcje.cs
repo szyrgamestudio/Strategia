7ef3e992bbfbbc77d48856f275359c3abdb2fcea
SzefuKox123PL <szyrrogi@hotmail.com>
2024-04-02 13:37:57 +0200

    opcje

--- Assets/Script/Opcje.cs
+++ Assets/Script/Opcje.cs
@@ -2,12 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Opcje : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    public GameObject opcje;
+    public GameObject tworcy;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for(int i = 0;i<resolutions.Length;i++)
+        {
+            string option  = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex; // Ustawianie wartości dropdown na aktualną rozdzielczość
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
     public void SetVolume(float volume)
     {
         Debug.Log(volume);
+        audioMixer.SetFloat("volume", volume);
     }
 
 
@@ -15,4 +51,25 @@ public class Opcje : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void opcjeOn()
+    {
+        if(opcje.activeSelf)
+            opcje.SetActive(false);
+        else
+            opcje.SetActive(true);
+    }
+        public void tworcyOn()
+    {
+        if(tworcy.activeSelf)
+            tworcy.SetActive(false);
+        else
+            tworcy.SetActive(true);
+    }
 }
+
